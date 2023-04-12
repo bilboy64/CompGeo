71038af58ae01ffd44e07f81ece1d86c488c9e90
bilboy64 <74872104+bilboy64@users.noreply.github.com>
2023-04-12 03:16:53 -0700

    Add files via upload

---
+++ hull.py
@@ -0,0 +1,34 @@
+import sys 
+
+# NOTES: 
+# 1. The following program calculates convex hull of a randomly generated set (list) of elements of R^2 (and R^3).
+# Every algorithm is defined as a function for organization purposes. 
+
+# 2. According to the algorithm the user desires, the program calculates and prints the convex hull's vertices as its
+# output. After every convex hull computation, we allow the user to re-enter an algorithm name of his choosing, if he 
+# wants to test the correctness of each output. 
+
+# 3. Terminate the program by either typing in "No" when asked if you want to continue, or by simply hitting ctrl + C.
+
+
+
+# Implementation part 1
+print("\n\t\t Convex Hull Algorithms \n\n")
+
+
+# 1. Graham's Scan Algorithm 
+
+def ConvexHullGS(points = []):
+    hull = []       # Defining hull as an empty list of vertices
+    print("I am a function")
+    
+    
+    
+    
+# Main function 
+def main():
+    print("Main function")
+
+if __name__ == "__main__":
+    main()
+    
